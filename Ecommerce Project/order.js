3c08c7031e70cedfcde24937e0eab42e029fd3a0
R2H_2 <r2h_2@MacBook-Pro.local>
2020-03-31 12:38:11 -0400

    added ecom project

---
+++ Ecommerce Project/order.js
@@ -0,0 +1,31 @@
+
+document.getElementById("product").addEventListener("click", function(){
+    
+    document.getElementById("div1t").style.gridArea = "3 / 3 / 4 / 4"; 
+    document.getElementById("div2t").style.gridArea = "4 / 1 / 5 / 2"; 
+    document.getElementById("div3t").style.gridArea = "4 / 2 / 5 / 3"; 
+    document.getElementById("div4t").style.gridArea = "4 / 3 / 5 / 4";
+    
+    document.getElementById("div9t").style.gridArea = "1 / 1 / 2 / 2"; 
+    document.getElementById("div10t").style.gridArea = "1 / 2 / 2 / 3"; 
+    document.getElementById("div11t").style.gridArea = "1 / 3 / 2 / 4"; 
+    document.getElementById("div12t").style.gridArea = "2 / 1 / 3 / 2";
+
+});
+
+document.getElementById("price").addEventListener("click", function(){
+    
+    document.getElementById("div1t").style.gridArea = "2 / 2 / 3 / 3"; 
+    document.getElementById("div2t").style.gridArea = "2 / 3 / 3 / 4"; 
+    document.getElementById("div3t").style.gridArea = "3 / 3 / 4 / 4"; 
+    document.getElementById("div4t").style.gridArea = "4 / 3 / 5 / 4";
+    document.getElementById("div5t").style.gridArea = "1 / 1 / 2 / 2"; 
+    document.getElementById("div6t").style.gridArea = "1 / 2 / 2 / 3"; 
+    document.getElementById("div7t").style.gridArea = "3 / 2 / 4 / 3"; 
+    document.getElementById("div8t").style.gridArea = "4 / 2 / 5 / 3";
+    document.getElementById("div9t").style.gridArea = "1 / 3 / 2 / 4"; 
+    document.getElementById("div10t").style.gridArea = "2 / 1 / 3 / 2"; 
+    document.getElementById("div11t").style.gridArea = "3 / 1 / 4 / 2"; 
+    document.getElementById("div12t").style.gridArea = "4 / 1 / 5 / 2";
+
+});
